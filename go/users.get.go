f38d1f4712bd1b3d43d007c33730cd9c8645b9b6
peko <gasu.basu@gmail.com>
2017-03-07 18:26:38 +0300

    update

--- go/users.get.go
+++ go/users.get.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"compress/gzip"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -10,7 +11,26 @@ import (
 	"os"
 	"runtime"
 	"strconv"
+	"time"
 )
+
+const fetchers = 800
+
+const max_uid = 10000000
+const max_cnt = 800
+const max_len = 4000
+
+const fields = "sex,country,city,bdate"
+
+type User struct {
+	uid         uint32
+	first_name  string
+	second_name string
+	bdate       string
+	sex         int
+	city        int
+	countyr     int
+}
 
 // fetcher
 func fetcher(urls <-chan string, out chan<- string) {
@@ -23,16 +43,16 @@ func fetcher(urls <-chan string, out chan<- string) {
 	// defer close(out)
 
 	req, _ := http.NewRequest("GET", "", nil)
-	req.Header.Add("Connection", "Keep-Alive")
+	req.Header.Add("Connection", "keep-alive")
 	req.Header.Add("Accept-Encoding", "gzip")
 
 	// defer transport.CancelRequest(req)
 
 	for path := range urls {
-		fmt.Println(path)
+
 		req.URL, _ = url.Parse(path)
 		res, err := client.Do(req)
-		fmt.Println(err, path)
+
 		if err != nil || res.StatusCode != 200 {
 			fmt.Println("[ ERR ]", err, res.Status)
 			continue
@@ -43,7 +63,14 @@ func fetcher(urls <-chan string, out chan<- string) {
 		reader.Close()
 		res.Body.Close()
 
-		out <- string(data)
+		jsonData := map[string]interface{}{}
+		json.Unmarshal(data, &jsonData)
+		resp := jsonData["response"].([]interface{})
+		for _, user := range resp {
+			u := user.(map[string]interface{})
+			line := fmt.Sprint(u["uid"], u["sex"], u["bdate"], u["city"], u["country"])
+			out <- line
+		}
 	}
 }
 
@@ -55,113 +82,49 @@ func feeder(lines chan<- string) {
 		if err != nil {
 			break
 		} else {
-			lines <- "https://api.vk.com/method/users.get?v=3&user_ids=" + string(line)
+			lines <- "https://api.vk.com/method/users.get?v=3&fields=" + fields + "&user_ids=" + string(line)
 		}
 	}
 }
 
-func feeder_2(lines chan<- string) {
-	for i := 0; i < 100; i += 1 {
-		lines <- "https://api.vk.com/method/users.get?v=3&user_ids=" + strconv.Itoa(i)
+func feeder_regular(urls chan<- string) {
+	defer close(urls)
+	uid := 0
+	cnt := 0
+	uids := ""
+	for {
+		uid = uid + 1
+		cnt = cnt + 1
+		if uid > max_uid {
+			break
+		}
+		uids = uids + "," + strconv.Itoa(uid)
+		if len(uids) > max_len || cnt > max_cnt {
+			urls <- "https://api.vk.com/method/users.get?v=3&fields=" + fields + "&user_ids=" + uids
+			uids = ""
+			cnt = 0
+		}
 	}
 }
-
-const fetchers = 200
 
 func main() {
 
 	runtime.GOMAXPROCS(runtime.NumCPU())
-
-	urls := make(chan string)
-	results := make(chan string)
-
-	go fetcher(urls, results)
-	go feeder_2(urls)
-
-	for res := range results {
-		fmt.Println(res)
+	urls := make(chan string, 1000)
+	results := make(chan string, 1000)
+	for i := 0; i < fetchers; i++ {
+		go fetcher(urls, results)
 	}
+	go feeder_regular(urls)
 
-}
-
-/*
-package main
-
-import (
-	"bufio"
-	"compress/gzip"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"net/url"
-	"os"
-	"strings"
-)
-
-// fetcher
-func fetcher(urls <-chan string, out chan<- string) {
-
-	transport := &http.Transport{}
-	client := &http.Client{Transport: transport}
-
-	// close all at the end
-	defer transport.CloseIdleConnections()
-	// defer close(out)
-
-	req, _ := http.NewRequest("GET", "", nil)
-	req.Header.Add("Connection", "Keep-Alive")
-	req.Header.Add("Accept-Encoding", "gzip")
-
-	// defer transport.CancelRequest(req)
-
-	for path := range urls {
-		req.URL, _ = url.Parse(path)
-		res, err := client.Do(req)
-		fmt.Println(res.Status, path)
-		if err != nil || res.StatusCode != 200 {
-			fmt.Println("[ ERR ]", err, res.Status)
-			continue
+	for {
+		select {
+		case res := <-results:
+			fmt.Println(res)
+		case <-time.After(time.Second * 5):
+			close(results)
+			return
 		}
-
-		reader, _ := gzip.NewReader(res.Body)
-		data, _ := ioutil.ReadAll(reader)
-		reader.Close()
-		res.Body.Close()
-
-		out <- string(data)
 	}
+
 }
-
-func readFileByLine(path string, urls chan<- string) {
-	inFile, _ := os.Open(path)
-	defer inFile.Close()
-	defer close(urls)
-	scanner := bufio.NewScanner(inFile)
-	scanner.Split(bufio.ScanLines)
-
-	prefix := "http://webcache.googleusercontent.com/search?q=cache:otzovik.com"
-	// prefix := "http://otzovik.com"
-	for scanner.Scan() {
-		urls <- prefix + strings.Split(scanner.Text(), "\t")[0]
-	}
-}
-
-func main() {
-	// runtime.GOMAXPROCS(runtime.NumCPU())
-	urls := make(chan string)
-	results := make(chan string)
-
-	go readFileByLine("uids", urls)
-	go fetcher(urls, results)
-
-	out_f, _ := os.Create("./output.txt")
-
-	defer out_f.Close()
-
-	for result := range results {
-		qqbytes, _ := out_f.WriteString(result)
-		fmt.Println("writed %d bytes", bytes)
-		out_f.Sync()
-	}
-}
-*/
